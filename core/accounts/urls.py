a9a019f27da8c676e2bfffb8b38f3e12e809dd3f
mow09 <donj31313@gmail.com>
2021-01-06 18:32:19 +0100

    login logout register - html - ready for user and manager

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import registerPage
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('register/', registerPage, name='register'),
+    path('', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+
+    # path('login/',
+    #     auth_views.LoginView.as_view(
+    #         template_name='registration/login.html'
+    #     ),
+    #     name='login'),
+
 ]
--- core/core/urls.py
+++ core/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    # admin comes last - no overwrite of logout
+    path('admin/', admin.site.urls),
 ]
